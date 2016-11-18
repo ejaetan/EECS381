c40502463247862dc51408548dc6c3f0767510b9
Yi Jie Tan <ms_ejae@hotmail.com>
2016-11-17 19:31:09 -0800

    Added Person_test.c - Tested functions: print_Person, destroy_Person, create_Person, get_Person_lastname

--- 1/project1/project1/Person_test.c
+++ 1/project1/project1/Person_test.c
@@ -7,3 +7,26 @@
 //
 
 #include <stdio.h>
+#include "Person.h"
+#include "Ordered_container.h"
+#include "Utility.h"
+
+
+int cmp_person_lastname(char* person1, char* person2);
+
+int main() {
+    
+    struct Person* p1 = create_Person("Eugene", "Goh", "734");
+    struct Ordered_container* container1 = OC_create_container((OC_comp_fp_t) cmp_person_lastname);
+    OC_insert(container1, p1);
+    print_Person(p1);
+    printf("%s\n", get_Person_lastname(p1));
+    destroy_Person(p1);
+    //printf("%s\n", get_Person_lastname(p1));
+}
+
+
+
+int cmp_person_lastname(char* person1, char* person2) {
+    return 0;
+}
